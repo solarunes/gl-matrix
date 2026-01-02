fc1bbbba78acca2cfda66f4ebd1efc03272bf9b0
solarunes <solarunes@protonmail.com>
2026-01-02 02:27:36 +0100

    fixed formatting

--- src/mat4.js
+++ src/mat4.js
@@ -228,25 +228,25 @@ export function identity(out) {
 export function transpose(out, a) {
   // If we are transposing ourselves we can skip a few steps but have to cache some values
   if (out === a) {
-  let a01 = a[1],
+  	let a01 = a[1],
     a02 = a[2],
     a03 = a[3];
-  let a12 = a[6],
+  	let a12 = a[6],
     a13 = a[7];
-  let a23 = a[11];
+  	let a23 = a[11];
 
-  out[1] = a[4];
-  out[2] = a[8];
-  out[3] = a[12];
-  out[4] = a01;
-  out[6] = a[9];
-  out[7] = a[13];
-  out[8] = a02;
-  out[9] = a12;
-  out[11] = a[14];
-  out[12] = a03;
-  out[13] = a13;
-  out[14] = a23;
+	out[1] = a[4];
+	out[2] = a[8];
+	out[3] = a[12];
+	out[4] = a01;
+	out[6] = a[9];
+	out[7] = a[13];
+	out[8] = a02;
+	out[9] = a12;
+	out[11] = a[14];
+	out[12] = a03;
+	out[13] = a13;
+	out[14] = a23;
   } else {
     out[0] = a[0];
     out[1] = a[4];
@@ -263,7 +263,7 @@ export function transpose(out, a) {
     out[12] = a[3];
     out[13] = a[7];
     out[14] = a[11];
-  out[15] = a[15];
+  	out[15] = a[15];
   }
 
   return out;
